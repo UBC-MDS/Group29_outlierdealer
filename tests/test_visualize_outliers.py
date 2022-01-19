bf2040cff525766beff489fea4be4af7a7c262b9
scarlqq <song.qingqing1001@gmail.com>
2022-01-18 22:42:20 -0800

    Fix typo in test_visualize_outliers.py

--- tests/test_visualize_outliers.py
+++ tests/test_visualize_outliers.py
@@ -1,12 +1,12 @@
 from outliers.visualize_outliers import visualize_outliers
 import pandas as pd
 import numpy as np
-import pytest
+from pytest import raises
 import altair as alt
 
 def df():
 
-    df =df = pd.DataFrame({"a":np.random.normal(100, 30, 100),
+    df = pd.DataFrame({"a":np.random.normal(100, 30, 100),
                   "b":np.random.normal(8, 5, size=100),
                   "c":np.random.randint(100, size=100),
                  "d":["A","B", "C", "D"] * 25})
@@ -15,19 +15,19 @@ def df():
 
 def test_visualize_outliers():
 
-    ## Test the type of return
-    result = visualize_outliers(df, columns = ['a', 'b', 'd'], type='violin')
+    ## Test the type of function result
+    dft = visualize_outliers(df(), columns = ['a', 'b', 'd'], type='violin')
     assert isinstance(
-        result, alt.vegalite.v4.api.FacetChart
+        dft, alt.vegalite.v4.api.FacetChart
     ), "Altair Chart object should be returned."
 
     ## Test the exceptions of invalid input
-    with pytest.raises(TypeError):
+    with raises(TypeError):
         visualize_outliers("572")
 
-    with pytest.raises(TypeError):
+    with raises(TypeError):
         visualize_outliers(df(), column = "gsgs")
 
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         visualize_outliers(df(), type='scatter')
         

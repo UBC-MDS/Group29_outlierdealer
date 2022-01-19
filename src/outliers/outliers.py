69e6bfc8271b2bd77bce3f1dc30a566b28b2ccae
Karanpreet Kaur <karanpreet088@gmail.com>
2022-01-18 19:19:05 -0800

    Add internal code of outlier_identification

--- src/outliers/outliers.py
+++ src/outliers/outliers.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 def outlier_identifier(dataframe, columns=None, identifier = 'IQR', return_df=False):
     """
     A function that identify and summarize the count and range of based on the method the user choose
@@ -39,3 +41,103 @@ def outlier_identifier(dataframe, columns=None, identifier = 'IQR', return_df=Fa
     lower_range                  NaN          NaN          NaN
     upper_range         (50.0, 54.0)         20.0          5.0
     """
+
+    # Handle dataframe type error (Check if dataframe is of type Pandas DataFrame)
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError(f"passed dataframe is of type {type(dataframe).__name__}, should be DataFrame")
+        
+    # Handle empty dataframe or dataframe with all NAN
+    if dataframe.empty or dataframe.dropna().empty:
+        raise ValueError("passed dataframe is None")
+    
+    # Handle columns type error (Check if columns are None or type list)
+    if not columns is None and not isinstance(columns, list):
+        raise TypeError(f"passed columns is of type {type(columns).__name__}, should be list or NoneType")
+    
+    # Handle identifier type error (Check if identifier is of type str)
+    if not isinstance(identifier, str):
+        raise TypeError(f"passed identifier is of type {type(identifier).__name__}, should be string with value 'Z_score' or 'IQR'")
+    
+    # Handle identifier Value error (Check if identifier has value 'Z_score' or 'IQR')
+    if identifier!='Z_score' and identifier!='IQR':
+        raise ValueError("passed identifier should have value 'Z_score' or 'IQR'")
+        
+    # Handle return_df type error (Check if identifier is of type bool)
+    if not isinstance(return_df, bool):
+        raise TypeError(f"passed return_df is of type {type(return_df).__name__}, should be bool with value as True or False")
+
+    if columns is None:
+        columns = df.columns
+    columns = dataframe.select_dtypes('number').columns
+    
+    df_selected = dataframe[columns]
+    df_selected['outlier'] = False
+    
+    output = pd.DataFrame(columns=columns, index=['outlier_count', 'outlier_percentage', 'mean', 'median', 'std', 'lower_range', 'upper_range'])
+    if identifier == 'Z_score':
+        if return_df == False:
+            for col in columns:
+                std = df_selected[col].std()
+                mean = df_selected[col].mean()
+                col_outliers = df_selected[np.abs(df_selected[col] - mean) > std*3]
+                output.loc['outlier_count', col] = len(col_outliers)
+                output.loc['outlier_percentage', col] = str(round(len(col_outliers) * 100 / len(df_selected), 2)) + '%'
+                output.loc['mean', col] = round(mean, 2)
+                output.loc['median', col] = round(np.percentile(df_selected[col], 50), 2)
+                output.loc['std', col] = round(std, 2)
+
+                low_range = col_outliers[col_outliers[col] < -std*3]
+                if not low_range.empty:
+                    if len(low_range) == 1:
+                        output.loc['lower_range', col] = min(low_range[col])
+                    else:
+                        output.loc['lower_range', col] = (min(low_range[col]), max(low_range[col]))
+                upper_range = col_outliers[col_outliers[col] > std*3]
+                if not upper_range.empty:
+                    if len(upper_range) == 1:
+                        output.loc['upper_range', col] = min(upper_range[col])
+                    else:
+                        output.loc['upper_range', col] = (min(upper_range[col]), max(upper_range[col]))
+            return output
+        else:
+            for col in columns:
+                std = df_selected[col].std()
+                mean = df_selected[col].mean()
+                df_selected.loc[np.abs((df_selected[col] - mean)) > std*3, 'outlier'] = True
+            return df_selected
+
+
+    elif identifier == 'IQR':
+        if return_df == False:
+            for col in columns:
+                std = df_selected[col].std()
+                mean = df_selected[col].mean()
+                iqr = np.percentile(df_selected[col], 75) - np.percentile(df_selected[col], 25)
+                col_outliers = df_selected[np.abs((df_selected[col] - mean) / std) > 1.5*iqr]
+                output.loc['outlier_count', col] = len(col_outliers)
+                output.loc['outlier_percentage', col] = str(round(len(col_outliers) * 100 / len(df_selected), 2)) + '%'
+                output.loc['mean', col] = round(mean, 2)
+                output.loc['median', col] = round(np.percentile(df_selected[col], 50), 2)
+                output.loc['std', col] = round(std, 2)
+                low_range = col_outliers[col_outliers[col] < -1.5*iqr]
+                if not low_range.empty:
+                    if len(low_range) == 1:
+                        output.loc['lower_range', col] = min(low_range[col])
+                    else:
+                        output.loc['lower_range', col] = (min(low_range[col]), max(low_range[col]))
+                upper_range = col_outliers[col_outliers[col] > 1.5*iqr]
+                if not upper_range.empty:
+                    if len(upper_range) == 1:
+                        output.loc['upper_range', col] = min(upper_range[col])
+                    else:
+                        output.loc['upper_range', col] = (min(upper_range[col]), max(upper_range[col]))
+            return output
+
+        else:
+            for col in columns:
+                std = df_selected[col].std()
+                mean = df_selected[col].mean()
+                iqr = np.percentile(df_selected[col], 75) - np.percentile(df_selected[col], 25)
+                df_selected.loc[np.abs((df_selected[col] - mean) / std) > 1.5*iqr, 'outlier'] = True
+            
+            return df_selected
